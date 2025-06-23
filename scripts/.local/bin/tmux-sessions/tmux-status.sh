3f74026fdfaccae36905df9b62acd8b67b348279
Gabriel Kollet <gabrielkollet99@gmail.com>
2025-06-23 00:16:21 -0300

    Trying to customize status bar.

---
+++ scripts/.local/bin/tmux-sessions/tmux-status.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cwd="#[bg=magenta fg=black] #{pane_current_path} "
+sess="#[bg=green fg=black] [#S]"
+git status &> /dev/null
+if [ "$?" -ne 0 ]; then
+    echo "$sess $cwd"
+    exit 0
+fi
+
+branch="#[bg=yellow fg=black] ( 󰘬 $(git rev-parse --abbrev-ref HEAD) )"
+echo "$sess $branch $cwd"
