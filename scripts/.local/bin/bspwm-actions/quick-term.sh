c2f8ed66f5e7768a6d560d3dc5b52a463648b8ee
Gabriel Kollet <gabrielkollet99@gmail.com>
2025-11-02 22:32:58 -0300

    Added .py script to autoimport math module.

--- scripts/.local/bin/bspwm-actions/quick-term.sh
+++ scripts/.local/bin/bspwm-actions/quick-term.sh
@@ -7,4 +7,4 @@ bspc rule -a "$class" -o state=floating center=on rectangle=960x540+0+0
 
 # The following command has these hardcodes flags because by now this script
 # assumes `$TERMINAL` is Alacritty: (may change in the future!)
-"$TERMINAL" --class "$class" -e "$app"
+"$TERMINAL" --class "$class" -e $app
