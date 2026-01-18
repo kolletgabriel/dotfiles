9ce8c2b56f520d1b5b237bc6480422c53d879d83
Gabriel Kollet <gabrielkollet99@gmail.com>
2026-01-18 11:53:42 -0300

    Enabling <C-w> for deleting pairs.

--- nvim/.config/nvim/lua/plugins/autopairs.lua
+++ nvim/.config/nvim/lua/plugins/autopairs.lua
@@ -2,6 +2,8 @@ return {
     'windwp/nvim-autopairs',
     -- event = 'InsertEnter',  -- eager loading so `autolist.nvim` overrides the keymap for `<CR>`
     config = function()
-        require('nvim-autopairs').setup({})
+        require('nvim-autopairs').setup({
+            map_c_w = true
+        })
     end
 }
