4bd5053676f10e623ab715d3e5492022eea9a8ac
Gabriel Kollet <gabrielkollet99@gmail.com>
2026-05-11 09:06:26 -0300

    change textwidth to 80
    81 kept overflowing the viewport anyways

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,2 +1,2 @@
-vim.opt.textwidth = 81             -- max line length
+vim.opt.textwidth = 80             -- max line length
 vim.opt.formatoptions:append("t")  -- automatic break line in insert mode
